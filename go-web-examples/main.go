5e8fcc8d85472375a5cdf81e03f7e5b75f244a72
peabits <1602488786@qq.com>
2023-02-17 11:20:10 +0800

    update go-web-example.

--- go-web-examples/main.go
+++ go-web-examples/main.go
@@ -1,8 +1,28 @@
 package main
 
 import (
-	"hello-golang/go-web-examples/example"
+	"fmt"
 )
+
+func dotProduct[F ~float32 | ~float64](v1, v2 []F) F {
+	var s F
+	for i, x := range v1 {
+		y := v2[i]
+		s += x * y
+	}
+	return s
+}
+
+func dotProduct1[F float32 | float64](v1, v2 []F) F {
+	var s F
+	for i, x := range v1 {
+		y := v2[i]
+		s += x * y
+	}
+	return s
+}
+
+type floats float32
 
 func main() {
 	// example.HelloWorld()
@@ -11,5 +31,9 @@ func main() {
 
 	// example.Templates()
 
-	example.Websockets()
+	// example.Websockets()
+	v1 := []floats{1, 2, 3}
+	v2 := []floats{0, 1, 0}
+	fmt.Println(dotProduct(v1, v2))
+	fmt.Println(dotProduct1(v1, v2))
 }
